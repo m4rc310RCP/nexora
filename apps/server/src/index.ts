807429e0d6ff2f11be8dedf46a4fd6624e39e689
m4rc310 <mlsilva@coamo.com.br>
2026-08-13 19:05:59 -0300

    feat: load plugin through runtime

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,6 +2,28 @@ import {
 	createServer,
 } from "node:http";
 
+import {
+	PluginRuntime,
+} from "@nexora/plugin-runtime";
+
+import {
+	helloPlugin,
+} from "../../../plugins/hello/src/index.js";
+
+
+const runtime =
+	new PluginRuntime();
+
+
+runtime.register(
+	helloPlugin,
+);
+
+
+await runtime.activate(
+	"hello",
+);
+
 
 const server =
 	createServer(
@@ -9,6 +31,43 @@ const server =
 			request,
 			response,
 		) => {
+
+			if (
+				request.url ===
+				"/api/plugins"
+			) {
+
+				response.writeHead(
+					200,
+					{
+						"Content-Type":
+							"application/json",
+					},
+				);
+
+				response.end(
+					JSON.stringify(
+						runtime
+							.getAll()
+							.map(
+								plugin => ({
+									id:
+										plugin.id,
+
+									name:
+										plugin.name,
+
+									version:
+										plugin.version,
+								}),
+							),
+					),
+				);
+
+				return;
+
+			}
+
 
 			response.writeHead(
 				200,
